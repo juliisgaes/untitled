fd185234b1dcb3a306640e1ee2cd3ca2eaa0a5c5
juliisgaes <julianisgaes@gmail.com>
2026-01-10 20:11:15 -0700

    agregando menú de depuración y organizando mejor las funciones

--- untitled.c
+++ untitled.c
@@ -2,7 +2,55 @@
 
 #include <raylib.h>
 
-int menu(float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr) {
+// Probablemente en un futuro esto se encuentre en un archivo "header".
+int menu(bool* depuracion_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr);
+
+// Función principal.
+int main(void) {
+
+	// Inicializando variables.
+	bool depuracion = true;
+	int escena_actual = 1;
+	float posicion_jugador_x = 0, posicion_jugador_y = 450;
+
+	// Inicializando punteros.
+	bool* depuracion_ptr = &depuracion;
+	float* posicion_jugador_x_ptr = &posicion_jugador_x;
+	float* posicion_jugador_y_ptr = &posicion_jugador_y;
+
+	// Creando la ventana principal.
+	InitWindow(500, 500, "untitled - 0.1");
+
+	// Estableciendo el límite de FPS.
+	SetTargetFPS(60);
+
+	// Bucle para actualizar pantalla.
+	while (!WindowShouldClose()) {
+
+		// Comenzando a dibujar.
+		BeginDrawing();
+
+		// Limpiando el canvas.
+		ClearBackground(GRAY);
+
+		// Comprobación de escenas.
+		switch (escena_actual) {
+			case 1:
+				menu(depuracion_ptr, posicion_jugador_x_ptr, posicion_jugador_y_ptr);
+			break;
+		}
+
+		// Terminando de dibujar.
+		EndDrawing();
+
+	}
+
+	return 0;
+
+}
+
+// Menú inicial.
+int menu(bool* depuracion_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr) {
 
 	// Comprobando entradas del jugador.
 	if (IsKeyDown(KEY_D) && *posicion_jugador_x_ptr <= 450) {
@@ -29,44 +77,11 @@ int menu(float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr) {
 	// Creando al jugador.
 	DrawRectangle(*posicion_jugador_x_ptr, *posicion_jugador_y_ptr, 50, 50, WHITE);
 
-	return 0;
+	// Cosas para depuración.
+	if (*depuracion_ptr == true) {
 
-}
-
-int main(void) {
-
-	// Inicializando variables.
-	int escena_actual = 1;
-	float posicion_jugador_x = 0, posicion_jugador_y = 450;
-
-	// Inicializando punteros.
-	float* posicion_jugador_x_ptr = &posicion_jugador_x;
-	float* posicion_jugador_y_ptr = &posicion_jugador_y;
-
-	// Creando la ventana principal.
-	InitWindow(500, 500, "untitled - 0.1");
-
-	// Estableciendo el límite de FPS.
-	SetTargetFPS(60);
-
-	// Bucle para actualizar pantalla.
-	while (!WindowShouldClose()) {
-
-		// Comenzando a dibujar.
-		BeginDrawing();
-
-		// Limpiando el canvas.
-		ClearBackground(GRAY);
-
-		// Comprobación de escenas.
-		switch (escena_actual) {
-			case 1:
-				menu(posicion_jugador_x_ptr, posicion_jugador_y_ptr);
-			break;
-		}
-
-		// Terminando de dibujar.
-		EndDrawing();
+		// Mostrando las coordenadas del jugador en pantalla.
+		DrawText(TextFormat("posición x del jugador: %f\nposición y del jugador: %f", *posicion_jugador_x_ptr, *posicion_jugador_y_ptr), 0, 0, 10, WHITE);
 
 	}
 
