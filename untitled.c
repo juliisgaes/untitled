58ee47af2b5d3ea5d3f81b7df053ca62b445a22e
juliisgaes <julianisgaes@gmail.com>
2026-01-19 15:59:55 -0700

    agregando recursos y funcionalidades nuevas

--- untitled.c
+++ untitled.c
@@ -3,14 +3,14 @@
 #include <raylib.h>
 
 // Probablemente en un futuro esto se encuentre en un archivo "header".
-int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D cursor);
+int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D* fondo_ptr, Texture2D* cursor_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr);
 
 // Función principal.
 int main(void) {
 
 	// Declarando variables.
 	bool depuracion = true;
-	unsigned int escena_actual;
+	unsigned int escena_actual = 0;
 
 	// Declarando punteros.
 	bool* depuracion_ptr = &depuracion;
@@ -36,23 +36,50 @@ int main(void) {
 
 			// Cargando menú.
 			case 0:
+
+				// Reservando memoria para coordenadas del jugador.
 				float* posicion_jugador_x_ptr = MemAlloc(sizeof(float));
 				float* posicion_jugador_y_ptr = MemAlloc(sizeof(float));
+
+				// Reservando memoria para hitboxes.
+				Rectangle* hitbox_jugador_ptr = MemAlloc(sizeof(Rectangle));
+				Rectangle* hitbox_salir_ptr = MemAlloc(sizeof(Rectangle));
+
+				// Reservando memoria para texturas.
+				Texture2D* fondo_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* cursor_ptr = MemAlloc(sizeof(Texture2D));
+
+				// Asignando valores a los datos anteriormente creados.
 				*posicion_jugador_y_ptr = 450;
-				Texture2D cursor = LoadTexture("recursos/cursor.png");
+				*fondo_ptr = LoadTexture("recursos/fondos/menu.png");
+				*cursor_ptr = LoadTexture("recursos/cursor.png");
+				hitbox_jugador_ptr->width = 50;
+				hitbox_jugador_ptr->height = 50;
+				hitbox_salir_ptr->x = 150;
+				hitbox_salir_ptr->y = 0;
+				hitbox_salir_ptr->width = 200;
+				hitbox_salir_ptr->height = 100;
+
+				// Cambiando de escena.
 				escena_actual++;
+
 			break;
 
 			// Ejecutando menú.
 			case 1:
-				menu(depuracion_ptr, escena_actual_ptr, posicion_jugador_x_ptr, posicion_jugador_y_ptr, cursor);
+				menu(depuracion_ptr, escena_actual_ptr, posicion_jugador_x_ptr, posicion_jugador_y_ptr, fondo_ptr, cursor_ptr, hitbox_jugador_ptr, hitbox_salir_ptr);
 			break;
 
-			// Descargando menú.
+			// Liberando memoria reservada para el menú.
 			case 2:
+				UnloadTexture(*fondo_ptr);
+				UnloadTexture(*cursor_ptr);
 				MemFree(posicion_jugador_x_ptr);
 				MemFree(posicion_jugador_y_ptr);
-				UnloadTexture(cursor);
+				MemFree(fondo_ptr);
+				MemFree(cursor_ptr);
+				MemFree(hitbox_jugador_ptr);
+				MemFree(hitbox_salir_ptr);
 			break;
 
 		}
@@ -67,7 +94,11 @@ int main(void) {
 }
 
 // Menú inicial.
-int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D cursor) {
+int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D* fondo_ptr, Texture2D* cursor_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr) {
+
+	// Definiendo hitbox del jugador.
+	hitbox_jugador_ptr->x = *posicion_jugador_x_ptr;
+	hitbox_jugador_ptr->y = *posicion_jugador_y_ptr;
 
 	// Comprobando entradas del jugador.
 	if (IsKeyDown(KEY_D) && *posicion_jugador_x_ptr <= 450) {
@@ -90,20 +121,25 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 		*posicion_jugador_y_ptr += 100 * GetFrameTime();
 
 	}
-	if (IsKeyDown(KEY_P)) {
+
+	// Comprobando colisiones.
+	if (CheckCollisionRecs(*hitbox_jugador_ptr, *hitbox_salir_ptr)) {
 		
 		*escena_actual_ptr = 2;
 
 	}
 
+	// Dibujando fondo.
+	DrawTexture(*fondo_ptr, 0, 0, WHITE);
+
 	// Creando al jugador.
-	DrawTexture(cursor, *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, WHITE);
+	DrawTexture(*cursor_ptr, *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, WHITE);
 
 	// Cosas para depuración.
 	if (*depuracion_ptr == true) {
 
 		// Mostrando las coordenadas del jugador en pantalla.
-		DrawText(TextFormat("posición x del jugador: %f\nposición y del jugador: %f", *posicion_jugador_x_ptr, *posicion_jugador_y_ptr), 0, 0, 10, WHITE);
+		DrawText(TextFormat("posición x del jugador: %f\nposición y del jugador: %f\nescena actual: %i", *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, *escena_actual_ptr), 10, 150, 12, BLACK);
 
 	}
 
