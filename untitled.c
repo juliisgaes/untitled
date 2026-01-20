85dd1ee1bb8209bbc66a07c7e73c063c321483d2
juliisgaes <julianisgaes@gmail.com>
2026-01-19 19:08:02 -0700

    ¡música!

--- untitled.c
+++ untitled.c
@@ -3,13 +3,13 @@
 #include <raylib.h>
 
 // Probablemente en un futuro esto se encuentre en un archivo "header".
-int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D* fondo_ptr, Texture2D* cursor_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr);
+int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* cursor_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr, float* temporizador_ptr, Music* musica_ptr);
 
 // Función principal.
 int main(void) {
 
 	// Declarando variables.
-	bool depuracion = true;
+	bool depuracion = false;
 	unsigned int escena_actual = 0;
 
 	// Declarando punteros.
@@ -18,6 +18,9 @@ int main(void) {
 
 	// Creando la ventana principal.
 	InitWindow(500, 500, "untitled - 0.1");
+
+	// Inicializando audio.
+	InitAudioDevice();
 
 	// Estableciendo el límite de FPS.
 	SetTargetFPS(60);
@@ -47,18 +50,31 @@ int main(void) {
 
 				// Reservando memoria para texturas.
 				Texture2D* fondo_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* fondo_2_ptr = MemAlloc(sizeof(Texture2D));
 				Texture2D* cursor_ptr = MemAlloc(sizeof(Texture2D));
+
+				// Reservando memoria para el temporizador.
+				float* temporizador_ptr = MemAlloc(sizeof(float));
+
+				// Reservando memoria para la música.
+				Music* musica_ptr = MemAlloc(sizeof(Music));
 
 				// Asignando valores a los datos anteriormente creados.
 				*posicion_jugador_y_ptr = 450;
 				*fondo_ptr = LoadTexture("recursos/fondos/menu.png");
+				*fondo_2_ptr = LoadTexture("recursos/fondos/menu_2.png");
 				*cursor_ptr = LoadTexture("recursos/cursor.png");
-				hitbox_jugador_ptr->width = 50;
-				hitbox_jugador_ptr->height = 50;
-				hitbox_salir_ptr->x = 150;
-				hitbox_salir_ptr->y = 0;
-				hitbox_salir_ptr->width = 200;
-				hitbox_salir_ptr->height = 100;
+				hitbox_jugador_ptr -> width = 50;
+				hitbox_jugador_ptr -> height = 50;
+				hitbox_salir_ptr -> x = 150;
+				hitbox_salir_ptr -> y = 0;
+				hitbox_salir_ptr -> width = 200;
+				hitbox_salir_ptr -> height = 100;
+				*temporizador_ptr = 0;
+				*musica_ptr = LoadMusicStream("recursos/musica/menu.mp3");
+
+				// Iniciando el stream de música.
+				PlayMusicStream(*musica_ptr);
 
 				// Cambiando de escena.
 				escena_actual++;
@@ -67,19 +83,25 @@ int main(void) {
 
 			// Ejecutando menú.
 			case 1:
-				menu(depuracion_ptr, escena_actual_ptr, posicion_jugador_x_ptr, posicion_jugador_y_ptr, fondo_ptr, cursor_ptr, hitbox_jugador_ptr, hitbox_salir_ptr);
+				menu(depuracion_ptr, escena_actual_ptr, posicion_jugador_x_ptr, posicion_jugador_y_ptr, fondo_ptr, fondo_2_ptr, cursor_ptr, hitbox_jugador_ptr, hitbox_salir_ptr, temporizador_ptr, musica_ptr);
 			break;
 
 			// Liberando memoria reservada para el menú.
 			case 2:
+				StopMusicStream(*musica_ptr);
 				UnloadTexture(*fondo_ptr);
+				UnloadTexture(*fondo_2_ptr);
 				UnloadTexture(*cursor_ptr);
 				MemFree(posicion_jugador_x_ptr);
 				MemFree(posicion_jugador_y_ptr);
 				MemFree(fondo_ptr);
+				MemFree(fondo_2_ptr);
 				MemFree(cursor_ptr);
 				MemFree(hitbox_jugador_ptr);
 				MemFree(hitbox_salir_ptr);
+				MemFree(temporizador_ptr);
+				UnloadMusicStream(*musica_ptr);
+				MemFree(musica_ptr);
 			break;
 
 		}
@@ -89,16 +111,22 @@ int main(void) {
 
 	}
 
+	// Apagando audio.
+	CloseAudioDevice();
+
+	// Cerrando ventana.
+	CloseWindow();
+
 	return 0;
 
 }
 
 // Menú inicial.
-int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D* fondo_ptr, Texture2D* cursor_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr) {
+int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* cursor_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr, float* temporizador_ptr, Music* musica_ptr) {
 
 	// Definiendo hitbox del jugador.
-	hitbox_jugador_ptr->x = *posicion_jugador_x_ptr;
-	hitbox_jugador_ptr->y = *posicion_jugador_y_ptr;
+	hitbox_jugador_ptr -> x = *posicion_jugador_x_ptr;
+	hitbox_jugador_ptr -> y = *posicion_jugador_y_ptr;
 
 	// Comprobando entradas del jugador.
 	if (IsKeyDown(KEY_D) && *posicion_jugador_x_ptr <= 450) {
@@ -129,17 +157,41 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 
 	}
 
+	// Administrando el temporizador.
+	if (*temporizador_ptr >= 2) {
+
+		*temporizador_ptr = 0;
+
+	}
+	else {
+		
+		*temporizador_ptr += 3 * GetFrameTime();
+	
+	}
+
 	// Dibujando fondo.
-	DrawTexture(*fondo_ptr, 0, 0, WHITE);
+	if (*temporizador_ptr > 1) {
+
+		DrawTexture(*fondo_2_ptr, 0, 0, WHITE);
+
+	}
+	else {
+	
+		DrawTexture(*fondo_ptr, 0, 0, WHITE);
+
+	}
 
 	// Creando al jugador.
 	DrawTexture(*cursor_ptr, *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, WHITE);
+
+	// Reproduciendo música.
+	UpdateMusicStream(*musica_ptr);
 
 	// Cosas para depuración.
 	if (*depuracion_ptr == true) {
 
 		// Mostrando las coordenadas del jugador en pantalla.
-		DrawText(TextFormat("posición x del jugador: %f\nposición y del jugador: %f\nescena actual: %i", *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, *escena_actual_ptr), 10, 150, 12, BLACK);
+		DrawText(TextFormat("posición x del jugador: %f\nposición y del jugador: %f\nescena actual: %i\ntemporizador: %f", *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, *escena_actual_ptr, *temporizador_ptr), 10, 150, 12, BLACK);
 
 	}
 
