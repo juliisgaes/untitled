0270f0e7f10ecdd1aadea76a53715319c9bcd94a
juliisgaes <julianisgaes@gmail.com>
2026-01-23 16:34:36 -0700

    mejorando el sistema de escenas

--- untitled.c
+++ untitled.c
@@ -3,18 +3,20 @@
 #include <raylib.h>
 
 // Declarando funciones antes de definirlas.
-int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Vector2* coordenadas_espacio_ptr, float* temporizador_fondo_ptr, float* temporizador_espacio_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* espacio_ptr, Texture2D* espacio_2_ptr, Texture2D* cursor_ptr, Music* musica_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr);
-int primerminijuego(unsigned int* escena_actual_ptr, float* temporizador_fondo_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr);
+int menu(unsigned int* escena_a_cambiar, unsigned int* escena_actual_ptr, float* temporizador_fondo_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* titulo_ptr, Texture2D* titulo_2_ptr);
+int tutorial(bool* depuracion_ptr, unsigned int* escena_a_cambiar, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Vector2* coordenadas_espacio_ptr, float* temporizador_fondo_ptr, float* escala_espacio_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* espacio_ptr, Texture2D* espacio_2_ptr, Texture2D* cursor_ptr, Music* musica_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr);
+int primerminijuego(unsigned int* escena_a_cambiar, unsigned int* escena_actual_ptr, float* temporizador_fondo_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr);
 
 // Función principal.
 int main(void) {
 
 	// Declarando variables.
 	bool depuracion = false;
-	unsigned int escena_actual = 0;
+	unsigned int escena_a_cambiar = 0, escena_actual = 0;
 
 	// Declarando punteros.
 	bool* depuracion_ptr = &depuracion;
+	unsigned int* escena_a_cambiar_ptr = &escena_a_cambiar;
 	unsigned int* escena_actual_ptr = &escena_actual;
 
 	// Creando la ventana principal.
@@ -33,13 +35,60 @@ int main(void) {
 		BeginDrawing();
 
 		// Limpiando el canvas.
-		ClearBackground(GRAY);
+		ClearBackground(WHITE);
 
 		// Comprobación de escenas.
 		switch (escena_actual) {
 
-			// Cargando elementos.
+			// Cargando menú.
 			case 0:
+
+				// Reservando memoria para el temporizador.
+				float* menu_temporizador_fondo_ptr = MemAlloc(sizeof(float));
+	  
+				// Reservando memoria para texturas.
+				Texture2D* menu_fondo_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* menu_fondo_2_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* titulo_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* titulo_2_ptr = MemAlloc(sizeof(Texture2D));
+
+				// Asignando valores a los datos anteriormente creados.
+				*menu_temporizador_fondo_ptr = 0;
+				*menu_fondo_ptr = LoadTexture("recursos/fondos/menu.png");
+				*menu_fondo_2_ptr = LoadTexture("recursos/fondos/menu_2.png");
+				*titulo_ptr = LoadTexture("recursos/sprites/titulo.png");
+				*titulo_2_ptr = LoadTexture("recursos/sprites/titulo_2.png");
+
+				// Cambiando de escena.
+				escena_actual = 1;
+
+			break;
+
+			// Ejecutando menú.
+			case 1:
+				menu(escena_a_cambiar_ptr, escena_actual_ptr, menu_temporizador_fondo_ptr, menu_fondo_ptr, menu_fondo_2_ptr, titulo_ptr, titulo_2_ptr);
+			break;
+
+			// Liberando memoria reservada para el menú.
+			case 2:
+
+				UnloadTexture(*titulo_2_ptr);
+				UnloadTexture(*titulo_ptr);
+				UnloadTexture(*menu_fondo_2_ptr);
+				UnloadTexture(*menu_fondo_ptr);
+				MemFree(titulo_2_ptr);
+				MemFree(titulo_ptr);
+				MemFree(menu_fondo_2_ptr);
+				MemFree(menu_fondo_ptr);
+				MemFree(menu_temporizador_fondo_ptr);
+
+				// Cambiando de escena.
+				escena_actual = escena_a_cambiar;
+
+			break;
+
+			// Cargando tutorial.
+			case 3:
 
 				// Reservando memoria para coordenadas del jugador.
 				float* posicion_jugador_x_ptr = MemAlloc(sizeof(float));
@@ -49,16 +98,16 @@ int main(void) {
 				Vector2* coordenadas_espacio_ptr = MemAlloc(sizeof(Vector2));
 
 				// Reservando memoria para los temporizadores.
-				float* temporizador_fondo_ptr = MemAlloc(sizeof(float));
-				float* temporizador_espacio_ptr = MemAlloc(sizeof(float));
+				float* tutorial_temporizador_fondo_ptr = MemAlloc(sizeof(float));
+				float* escala_espacio_ptr = MemAlloc(sizeof(float));
 
 				// Reservando memoria para hitboxes.
 				Rectangle* hitbox_jugador_ptr = MemAlloc(sizeof(Rectangle));
 				Rectangle* hitbox_salir_ptr = MemAlloc(sizeof(Rectangle));
 
 				// Reservando memoria para texturas.
-				Texture2D* fondo_ptr = MemAlloc(sizeof(Texture2D));
-				Texture2D* fondo_2_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* tutorial_fondo_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* tutorial_fondo_2_ptr = MemAlloc(sizeof(Texture2D));
 				Texture2D* espacio_ptr = MemAlloc(sizeof(Texture2D));
 				Texture2D* espacio_2_ptr = MemAlloc(sizeof(Texture2D));
 				Texture2D* cursor_ptr = MemAlloc(sizeof(Texture2D));
@@ -70,10 +119,10 @@ int main(void) {
 				*posicion_jugador_y_ptr = 450;
 				coordenadas_espacio_ptr -> x = 250;
 				coordenadas_espacio_ptr -> y = 400;
-				*temporizador_fondo_ptr = 0;
-				*temporizador_espacio_ptr = 0;
-				*fondo_ptr = LoadTexture("recursos/fondos/menu.png");
-				*fondo_2_ptr = LoadTexture("recursos/fondos/menu_2.png");
+				*tutorial_temporizador_fondo_ptr = 0;
+				*escala_espacio_ptr = 0;
+				*tutorial_fondo_ptr = LoadTexture("recursos/fondos/tutorial.png");
+				*tutorial_fondo_2_ptr = LoadTexture("recursos/fondos/tutorial_2.png");
 				*espacio_ptr = LoadTexture("recursos/sprites/espacio.png");
 				*espacio_2_ptr = LoadTexture("recursos/sprites/espacio_2.png");
 				*cursor_ptr = LoadTexture("recursos/cursor.png");
@@ -89,47 +138,81 @@ int main(void) {
 				PlayMusicStream(*musica_ptr);
 
 				// Cambiando de escena.
-				escena_actual = 1;
+				escena_actual = 4;
 
 			break;
 
-			// Ejecutando menú.
-			case 1:
-				menu(depuracion_ptr, escena_actual_ptr, posicion_jugador_x_ptr, posicion_jugador_y_ptr, coordenadas_espacio_ptr, temporizador_fondo_ptr, temporizador_espacio_ptr, fondo_ptr, fondo_2_ptr, espacio_ptr, espacio_2_ptr, cursor_ptr, musica_ptr, hitbox_jugador_ptr, hitbox_salir_ptr);
+			// Ejecutando tutorial.
+			case 4:
+				tutorial(depuracion_ptr, escena_a_cambiar_ptr, escena_actual_ptr, posicion_jugador_x_ptr, posicion_jugador_y_ptr, coordenadas_espacio_ptr, tutorial_temporizador_fondo_ptr, escala_espacio_ptr, tutorial_fondo_ptr, tutorial_fondo_2_ptr, espacio_ptr, espacio_2_ptr, cursor_ptr, musica_ptr, hitbox_jugador_ptr, hitbox_salir_ptr);
 			break;
 
-			// Liberando memoria reservada para el menú.
-			case 2:
+			// Liberando memoria reservada para el tutorial.
+			case 5:
+
 				StopMusicStream(*musica_ptr);
 				UnloadTexture(*cursor_ptr);
-				UnloadTexture(*espacio_ptr);
 				UnloadTexture(*espacio_2_ptr);
+				UnloadTexture(*espacio_ptr);
+				UnloadTexture(*tutorial_fondo_2_ptr);
+				UnloadTexture(*tutorial_fondo_ptr);
 				UnloadMusicStream(*musica_ptr);
-				MemFree(posicion_jugador_x_ptr);
-				MemFree(posicion_jugador_y_ptr);
-				MemFree(coordenadas_espacio_ptr);
-				MemFree(temporizador_espacio_ptr);
-				MemFree(espacio_ptr);
-				MemFree(espacio_2_ptr);
-				MemFree(cursor_ptr);
 				MemFree(musica_ptr);
-				MemFree(hitbox_jugador_ptr);
+				MemFree(cursor_ptr);
+				MemFree(espacio_2_ptr);
+				MemFree(espacio_ptr);
+				MemFree(tutorial_fondo_2_ptr);
+				MemFree(tutorial_fondo_ptr);
 				MemFree(hitbox_salir_ptr);
-				escena_actual = 3;
+				MemFree(hitbox_jugador_ptr);
+				MemFree(escala_espacio_ptr);
+				MemFree(tutorial_temporizador_fondo_ptr);
+				MemFree(coordenadas_espacio_ptr);
+				MemFree(posicion_jugador_y_ptr);
+				MemFree(posicion_jugador_x_ptr);
+
+				// Cambiando de escena.
+				escena_actual = escena_a_cambiar;
+
+			break;
+
+			// Cargando primer mini juego.
+			case 6:
+		
+				// Reservando memoria para el temporizador.
+				float* primerminijuego_temporizador_fondo_ptr = MemAlloc(sizeof(float));
+
+				// Reservando memoria para texturas.
+				Texture2D* primerminijuego_fondo_ptr = MemAlloc(sizeof(Texture2D));
+				Texture2D* primerminijuego_fondo_2_ptr = MemAlloc(sizeof(Texture2D));
+
+				// Asignando valores a los datos anteriormente creados.
+				*primerminijuego_temporizador_fondo_ptr = 0;
+				*primerminijuego_fondo_ptr = LoadTexture("recursos/fondos/tutorial.png");
+				*primerminijuego_fondo_2_ptr = LoadTexture("recursos/fondos/tutorial_2.png");
+
+				// Cambiando de escena.
+				escena_actual = 7;
+
 			break;
 
 			// Ejecutando primer mini juego.
-			case 3:
-				primerminijuego(escena_actual_ptr, temporizador_fondo_ptr, fondo_ptr, fondo_2_ptr);
+			case 7:
+				primerminijuego(escena_a_cambiar_ptr, escena_actual_ptr, primerminijuego_temporizador_fondo_ptr, primerminijuego_fondo_ptr, primerminijuego_fondo_2_ptr);
 			break;
 
 			// Liberando memoria reservada para el primer mini juego.
-			case 4:
-				UnloadTexture(*fondo_ptr);
-				UnloadTexture(*fondo_2_ptr);
-				MemFree(temporizador_fondo_ptr);
-				MemFree(fondo_ptr);
-				MemFree(fondo_2_ptr);
+			case 8:
+
+				UnloadTexture(*primerminijuego_fondo_2_ptr);
+				UnloadTexture(*primerminijuego_fondo_ptr);
+				MemFree(primerminijuego_fondo_2_ptr);
+				MemFree(primerminijuego_fondo_ptr);
+				MemFree(primerminijuego_temporizador_fondo_ptr);
+
+				// Cambiando de escena.
+				escena_actual = escena_a_cambiar;
+
 			break;
 
 		}
@@ -149,8 +232,50 @@ int main(void) {
 
 }
 
-// Menú inicial.
-int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Vector2* coordenadas_espacio_ptr, float* temporizador_fondo_ptr, float* temporizador_espacio_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* espacio_ptr, Texture2D* espacio_2_ptr, Texture2D* cursor_ptr, Music* musica_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr) {
+// Menú.
+int menu(unsigned int* escena_a_cambiar_ptr, unsigned int* escena_actual_ptr, float* temporizador_fondo_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* titulo_ptr, Texture2D* titulo_2_ptr) {
+
+	// Comprobando entradas del jugador.
+	if (IsKeyDown(KEY_P)) {
+	
+		*escena_a_cambiar_ptr = 3;
+		*escena_actual_ptr = 2;
+	
+	}
+	if (IsKeyDown(KEY_L)) {
+	
+		*escena_a_cambiar_ptr = 6;
+		*escena_actual_ptr = 2;
+	
+	}
+
+	// Administrando temporizador fondo.
+	if (*temporizador_fondo_ptr >= 2) {
+
+		*temporizador_fondo_ptr = 0;
+
+	}
+
+	// Administrando fondos.
+	if (*temporizador_fondo_ptr > 1) {
+		
+		DrawTexture(*fondo_2_ptr, 0, 0, WHITE);
+		*temporizador_fondo_ptr += 2 * GetFrameTime();
+	
+	}
+	else {
+		
+		DrawTexture(*fondo_ptr, 0, 0, WHITE);
+		*temporizador_fondo_ptr += 2 * GetFrameTime();
+
+	}
+
+	return 0;
+
+}
+
+// Tutorial.
+int tutorial(bool* depuracion_ptr, unsigned int* escena_a_cambiar_ptr, unsigned int* escena_actual_ptr, float* posicion_jugador_x_ptr, float* posicion_jugador_y_ptr, Vector2* coordenadas_espacio_ptr, float* temporizador_fondo_ptr, float* escala_espacio_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr, Texture2D* espacio_ptr, Texture2D* espacio_2_ptr, Texture2D* cursor_ptr, Music* musica_ptr, Rectangle* hitbox_jugador_ptr, Rectangle* hitbox_salir_ptr) {
 
 	// Definiendo hitbox del jugador.
 	hitbox_jugador_ptr -> x = *posicion_jugador_x_ptr;
@@ -203,20 +328,21 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 	if (CheckCollisionRecs(*hitbox_jugador_ptr, *hitbox_salir_ptr) && *temporizador_fondo_ptr > 1) {
 
 		// Mostrar el sprite de espacio.
-		DrawTextureEx(*espacio_ptr, *coordenadas_espacio_ptr, 0, *temporizador_espacio_ptr, WHITE);
+		DrawTextureEx(*espacio_ptr, *coordenadas_espacio_ptr, 0, *escala_espacio_ptr, WHITE);
 
-		if (*temporizador_espacio_ptr <= 1 && coordenadas_espacio_ptr -> x >= 100) {
+		if (*escala_espacio_ptr <= 1 && coordenadas_espacio_ptr -> x >= 100) {
 		
 			// Reducir las coordenadas en eje x para mantener centrado el sprite.
 			coordenadas_espacio_ptr -> x -= 150 * GetFrameTime();
 
 			// Aumentar el temporizador del sprite de espacio.
-			*temporizador_espacio_ptr += GetFrameTime();
+			*escala_espacio_ptr += GetFrameTime();
 
 		}
 		if (IsKeyDown(KEY_SPACE)) {
 		
-			*escena_actual_ptr = 2;
+			*escena_a_cambiar_ptr = 0;
+			*escena_actual_ptr = 5;
 		
 		}
 
@@ -224,21 +350,22 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 	if (CheckCollisionRecs(*hitbox_jugador_ptr, *hitbox_salir_ptr) && *temporizador_fondo_ptr < 1) {
 	
 		// Mostrar el otro sprite de espacio.
-		DrawTextureEx(*espacio_2_ptr, *coordenadas_espacio_ptr, 0, *temporizador_espacio_ptr, WHITE);
+		DrawTextureEx(*espacio_2_ptr, *coordenadas_espacio_ptr, 0, *escala_espacio_ptr, WHITE);
 
-		if (*temporizador_espacio_ptr <= 1 && coordenadas_espacio_ptr -> x >= 100) {
+		if (*escala_espacio_ptr <= 1 && coordenadas_espacio_ptr -> x >= 100) {
 
 			// Reducir las coordenadas en eje x para mantener centrado el sprite.
 			coordenadas_espacio_ptr -> x -= 150 * GetFrameTime();
 
 			// Aumentar el temporizador del sprite de espacio.
-			*temporizador_espacio_ptr += GetFrameTime();
+			*escala_espacio_ptr += GetFrameTime();
 		
 		}
 		// Cambiar escena al presionar espacio.
 		if (IsKeyDown(KEY_SPACE)) {
 		
-			*escena_actual_ptr = 2;
+			*escena_a_cambiar_ptr = 0;
+			*escena_actual_ptr = 5;
 		
 		}
 	
@@ -247,13 +374,13 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 	if (!CheckCollisionRecs(*hitbox_jugador_ptr, *hitbox_salir_ptr) && *temporizador_fondo_ptr > 1) {
 	
 		// Animación de salida del sprite de espacio.
-		if (*temporizador_espacio_ptr >= 0 && coordenadas_espacio_ptr -> x < 250) {
+		if (*escala_espacio_ptr >= 0 && coordenadas_espacio_ptr -> x < 250) {
 
-			DrawTextureEx(*espacio_ptr, *coordenadas_espacio_ptr, 0, *temporizador_espacio_ptr, WHITE);
+			DrawTextureEx(*espacio_ptr, *coordenadas_espacio_ptr, 0, *escala_espacio_ptr, WHITE);
 		
 			coordenadas_espacio_ptr -> x += 150 * GetFrameTime();
 
-			*temporizador_espacio_ptr -= GetFrameTime();
+			*escala_espacio_ptr -= GetFrameTime();
 		
 		}
 		
@@ -261,13 +388,13 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 	if (!CheckCollisionRecs(*hitbox_jugador_ptr, *hitbox_salir_ptr) && *temporizador_fondo_ptr < 1) {
 	
 		// Animación de salida del sprite de espacio.
-		if (*temporizador_espacio_ptr >= 0 && coordenadas_espacio_ptr -> x < 250) {
+		if (*escala_espacio_ptr >= 0 && coordenadas_espacio_ptr -> x < 250) {
 
-			DrawTextureEx(*espacio_2_ptr, *coordenadas_espacio_ptr, 0, *temporizador_espacio_ptr, WHITE);
+			DrawTextureEx(*espacio_2_ptr, *coordenadas_espacio_ptr, 0, *escala_espacio_ptr, WHITE);
 		
 			coordenadas_espacio_ptr -> x += 150 * GetFrameTime();
 
-			*temporizador_espacio_ptr -= GetFrameTime();
+			*escala_espacio_ptr -= GetFrameTime();
 		
 		}
 		
@@ -283,7 +410,7 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 	if (*depuracion_ptr == true) {
 
 		// Mostrando las coordenadas del jugador en pantalla.
-		DrawText(TextFormat("posición x del jugador: %f\nposición y del jugador: %f\nescena actual: %i\ntemporizador fondo: %f\ntemporizador espacio: %f", *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, *escena_actual_ptr, *temporizador_fondo_ptr, *temporizador_espacio_ptr), 10, 150, 12, BLACK);
+		DrawText(TextFormat("posición x del jugador: %f\nposición y del jugador: %f\nescena actual: %i\ntemporizador fondo: %f\ntemporizador espacio: %f", *posicion_jugador_x_ptr, *posicion_jugador_y_ptr, *escena_actual_ptr, *temporizador_fondo_ptr, *escala_espacio_ptr), 10, 150, 12, BLACK);
 
 	}
 
@@ -292,28 +419,31 @@ int menu(bool* depuracion_ptr, unsigned int* escena_actual_ptr, float* posicion_
 }
 
 // Primer minijuego.
-int primerminijuego(unsigned int* escena_actual_ptr, float* temporizador_fondo_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr) {
+int primerminijuego(unsigned int* escena_a_cambiar, unsigned int* escena_actual_ptr, float* temporizador_fondo_ptr, Texture2D* fondo_ptr, Texture2D* fondo_2_ptr) {
 
 	// Comprobando entradas del jugador.
 	if (IsKeyDown(KEY_P)) {
 		
-		*escena_actual_ptr = 4;
+		*escena_a_cambiar = 0;
+		*escena_actual_ptr = 8;
 
 	}
 
-	// Administrando temporizador de fondo.
-	if (*temporizador_fondo_ptr > 2) {
+	// Administrando temporizador fondo.
+	if (*temporizador_fondo_ptr >= 2) {
 
 		*temporizador_fondo_ptr = 0;
 
 	}
-	if (*temporizador_fondo_ptr > 1 && *temporizador_fondo_ptr < 2) {
+
+	// Administrando fondos.
+	if (*temporizador_fondo_ptr > 1) {
 		
 		DrawTexture(*fondo_2_ptr, 0, 0, WHITE);
 		*temporizador_fondo_ptr += 2 * GetFrameTime();
 	
 	}
-	if (*temporizador_fondo_ptr < 1) {
+	else {
 		
 		DrawTexture(*fondo_ptr, 0, 0, WHITE);
 		*temporizador_fondo_ptr += 2 * GetFrameTime();
